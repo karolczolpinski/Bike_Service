a5325ab71106cff5c832b3cd8c58220dfee8f449
MrSigma123 <karol.czolpinski@gmail.com>
2026-05-08 21:24:18 +0200

    Add sampe data - users, parts, bikes, registrations (layout in Polish)

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from .models import Zgloszenie, Rower, Czesc
+from .models import Uzytkownik, Rower, Zgloszenie, Czesc
 
 
 def home(request):
     context = {
-        'liczba_zgloszen': Zgloszenie.objects.count(),
+        'liczba_uzytkownikow': Uzytkownik.objects.count(),
         'liczba_rowerow': Rower.objects.count(),
+        'liczba_zgloszen': Zgloszenie.objects.count(),
         'liczba_czesci': Czesc.objects.count(),
     }
+
     return render(request, 'home.html', context)
 
 def rowery(request):
