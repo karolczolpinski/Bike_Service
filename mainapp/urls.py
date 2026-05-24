ccf13d55e122c2e7308d0fdad081d971838c2a5c
MrSigma123 <karol.czolpinski@gmail.com>
2026-05-24 23:32:04 +0200

    Add urls for new forms and views

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -22,4 +22,20 @@ urlpatterns = [
     path('panel-admin/uzytkownicy/dodaj/', views.dodaj_uzytkownika, name='dodaj_uzytkownika'),
     
     path('rejestracja/', views.rejestracja, name='rejestracja'),
+    
+    path('panel-klienta/', views.panel_klienta, name='panel_klienta'),
+    path('panel-mechanika/', views.panel_mechanika, name='panel_mechanika'),
+    path('panel-magazyniera/', views.panel_magazyniera, name='panel_magazyniera'),
+
+    path('zlecenia/', views.zlecenia, name='zlecenia'),
+    path('zlecenia/przyjmij/<int:zlecenie_id>/', views.przyjmij_zlecenie, name='przyjmij_zlecenie'),
+
+    path('diagnozy/dodaj/', views.dodaj_diagnoze, name='dodaj_diagnoze'),
+    path('raporty/dodaj/', views.dodaj_raport, name='dodaj_raport'),
+    path('zuzyte-czesci/dodaj/', views.dodaj_zuzyta_czesc, name='dodaj_zuzyta_czesc'),
+
+    path('czesci/dodaj/', views.dodaj_czesc, name='dodaj_czesc'),
+
+    path('zamowienia-czesci/', views.zamowienia_czesci, name='zamowienia_czesci'),
+    path('zamowienia-czesci/dodaj/', views.dodaj_zamowienie_czesci, name='dodaj_zamowienie_czesci'),
 ]
